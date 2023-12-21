372cfaf3d2e9f4644ae6731b0715ec66a7056e9b
Tanya Holikava <holikavadev@gmail.com>
2023-12-21 22:35:56 +0300

    do other tasks

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,10 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getSumOfDigits(n) {
+  let arrNum = n.toString().split('');
+  let sum = 0;
+
+  for ( let i = 0; i < arrNum.length; i += 1) {
+    sum += +(arrNum[i]);
+  }
+
+  return sum < 10 ? sum : getSumOfDigits(sum);
 }
+
 
 module.exports = {
   getSumOfDigits
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if ( !Array.isArray(arr)) {
+    return `'arr' parameter must be an instance of the Array!`;
+  }
+
+  let newArr = Array.from(arr);
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+
+  for (let i = 0; i < newArr.length; i += 1) {
+    let index = i;
+    if (newArr[i] === discardNext) {
+      newArr.splice(index, 2);
+    }
+    if (newArr[i] === discardPrev) {
+      newArr.splice(index - 1, 2);
+    }
+    if (newArr[i] === doubleNext) {
+      newArr.splice(index, 1, newArr[index + 1]);
+    }
+    if (newArr[i] === doublePrev) {
+      newArr.splice(index, 1, newArr[index - 1]);
+    }
+  }
+
+  return newArr;
 }
 
 module.exports = {
